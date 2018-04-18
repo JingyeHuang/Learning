a150c3867649a9894a785b09d6f5653bd36fc99e
JingyeHuang <307669280@qq.com>
2018-04-18 23:16:40 +0800

    modify Jpygame

--- PygameJeremy/Jpygame.py
+++ PygameJeremy/Jpygame.py
@@ -16,7 +16,8 @@ fullscreen = False
 
 screen = pygame.display.set_mode(size) 
 pygame.display.set_caption('Jgame')
-pika = pygame.image.load('pika.jpg')
+pika = pygame.image.load('pika.png')
+bg = pygame.image.load('bg.jpg')
 position = pika.get_rect()
 
 while True:
@@ -26,36 +27,30 @@ while True:
             
         if event.type == pygame.KEYDOWN:
             if event.key == K_LEFT:
-                if position.left > 0:
-                    speed = [-10,0]
-                else:
-                    speed = [0,0]
+                speed[0] = -10
                 
             if event.key == K_RIGHT:
-                if position.right < width:
-                    speed = [10,0]
-                else:
-                    speed = [0,0]
+                speed[0] = 10
                 
             if event.key == K_UP:
-                if position.top >0:
-                    speed = [0,-10]
-                else:
-                    speed = [0,0]
+                speed[1] = -10
                 
             if event.key == K_DOWN:
-                if position.bottom < height:
-                    speed = [0,10]
-                else:
-                    speed = [0,0]
+                speed[1] = 10
                 
         if event.type == pygame.KEYUP:
             speed = [0,0]
-        
+    
+    if (position.left < 0 and speed[0] < 0) or (position.right > width and speed[0] > 0):
+        speed[0] = 0
+    if (position.top < 0 and speed[1] < 0) or (position.bottom > height and speed[1] > 0):
+        speed[1] = 0
+    
     position = position.move(speed)
                 
     screen.fill([255,255,255])
     #refresh image
+    screen.blit(bg, (0,0))
     screen.blit(pika, position)
     #refresh screen
     pygame.display.flip()
