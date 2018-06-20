3598d2b9b66172c1fb5fa4c0d858d0a63dca3ca4
JingyeHuang <307669280@qq.com>
2018-06-20 21:25:10 +0800

    collide detection

--- plane/enermy.py
+++ plane/enermy.py
@@ -6,10 +6,14 @@ class SmallEnermy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = pygame.image.load('images/enemy1.png').convert_alpha()
+        self.destroy_images = []
+        self.destroy_images.extend([pygame.image.load('images/enemy1_down1.png').convert_alpha(), pygame.image.load('images/enemy1_down2.png').convert_alpha(), pygame.image.load('images/enemy1_down3.png').convert_alpha(), pygame.image.load('images/enemy1_down4.png').convert_alpha()])
         self.rect = self.image.get_rect()
         self.width, self.height = bg_size[0], bg_size[1]
         self.speed = 2
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-5 * self.height, 0)
+        self.mask = pygame.mask.from_surface(self.image)
         
     def move(self):
         if self.rect.top < self.height:
@@ -18,6 +22,7 @@ class SmallEnermy(pygame.sprite.Sprite):
             self.reset()
             
     def reset(self):
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-5 * self.height, 0)
         
         
@@ -26,10 +31,14 @@ class MidEnermy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = pygame.image.load('images/enemy2.png').convert_alpha()
+        self.destroy_images = []
+        self.destroy_images.extend([pygame.image.load('images/enemy2_down1.png').convert_alpha(), pygame.image.load('images/enemy2_down2.png').convert_alpha(), pygame.image.load('images/enemy2_down3.png').convert_alpha(), pygame.image.load('images/enemy2_down4.png').convert_alpha()])
         self.rect = self.image.get_rect()
         self.width, self.height = bg_size[0], bg_size[1]
         self.speed = 2
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-10 * self.height, - self.height)
+        self.mask = pygame.mask.from_surface(self.image)
         
     def move(self):
         if self.rect.top < self.height:
@@ -38,6 +47,7 @@ class MidEnermy(pygame.sprite.Sprite):
             self.reset()
             
     def reset(self):
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-10 * self.height, - self.height)
         
         
@@ -47,10 +57,14 @@ class BigEnermy(pygame.sprite.Sprite):
         
         self.image1 = pygame.image.load('images/enemy3_n1.png').convert_alpha()
         self.image2 = pygame.image.load('images/enemy3_n2.png').convert_alpha()
+        self.destroy_images = []
+        self.destroy_images.extend([pygame.image.load('images/enemy3_down1.png').convert_alpha(), pygame.image.load('images/enemy3_down2.png').convert_alpha(), pygame.image.load('images/enemy3_down3.png').convert_alpha(), pygame.image.load('images/enemy3_down4.png').convert_alpha(), pygame.image.load('images/enemy3_down5.png').convert_alpha(), pygame.image.load('images/enemy3_down6.png').convert_alpha()])
         self.rect = self.image1.get_rect()
         self.width, self.height = bg_size[0], bg_size[1]
-        self.speed = 2
+        self.speed = 1
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-15 * self.height, -5 * self.height)
+        self.mask = pygame.mask.from_surface(self.image1)
         
     def move(self):
         if self.rect.top < self.height:
@@ -59,4 +73,5 @@ class BigEnermy(pygame.sprite.Sprite):
             self.reset()
             
     def reset(self):
+        self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-15 * self.height, -5 * self.height)
--- plane/myplane.py
+++ plane/myplane.py
@@ -12,10 +12,14 @@ class MyPlane(pygame.sprite.Sprite):
         
         self.image = pygame.image.load('images/me1.png').convert_alpha()
         self.image2 = pygame.image.load('images/me2.png').convert_alpha()
+        self.destroy_images = []
+        self.destroy_images.extend([pygame.image.load('images/me_destroy_1.png').convert_alpha(), pygame.image.load('images/me_destroy_2.png').convert_alpha(), pygame.image.load('images/me_destroy_3.png').convert_alpha(), pygame.image.load('images/me_destroy_4.png').convert_alpha()])
         self.rect = self.image.get_rect()
         self.width, self.height = bg_size[0], bg_size[1]
         self.rect.left, self.rect.top = (self.width - self.rect.width) // 2, self.height - self.rect.height - 60
         self.speed = 10
+        self.active = True
+        self.mask = pygame.mask.from_surface(self.image) #将飞机非透明的部分标记为mask
         
     def moveUp(self):
         if self.rect.top > 0:
