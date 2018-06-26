94faf7b0fd27cde4ea5a31c8ef3011294009519c
JingyeHuang <307669280@qq.com>
2018-06-26 20:43:41 +0800

    bullet module

--- plane/enermy.py
+++ plane/enermy.py
@@ -27,10 +27,14 @@ class SmallEnermy(pygame.sprite.Sprite):
         
         
 class MidEnermy(pygame.sprite.Sprite):
+    
+    energy = 8
+    
     def __init__(self, bg_size):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = pygame.image.load('images/enemy2.png').convert_alpha()
+        self.image_hit = pygame.image.load('images/enemy2_hit.png').convert_alpha()
         self.destroy_images = []
         self.destroy_images.extend([pygame.image.load('images/enemy2_down1.png').convert_alpha(), pygame.image.load('images/enemy2_down2.png').convert_alpha(), pygame.image.load('images/enemy2_down3.png').convert_alpha(), pygame.image.load('images/enemy2_down4.png').convert_alpha()])
         self.rect = self.image.get_rect()
@@ -39,6 +43,8 @@ class MidEnermy(pygame.sprite.Sprite):
         self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-10 * self.height, - self.height)
         self.mask = pygame.mask.from_surface(self.image)
+        self.energy = MidEnermy.energy
+        self.hit = False
         
     def move(self):
         if self.rect.top < self.height:
@@ -48,15 +54,20 @@ class MidEnermy(pygame.sprite.Sprite):
             
     def reset(self):
         self.active = True
+        self.energy = MidEnermy.energy
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-10 * self.height, - self.height)
         
         
 class BigEnermy(pygame.sprite.Sprite):
+    
+    energy = 20
+    
     def __init__(self, bg_size):
         pygame.sprite.Sprite.__init__(self)
         
         self.image1 = pygame.image.load('images/enemy3_n1.png').convert_alpha()
         self.image2 = pygame.image.load('images/enemy3_n2.png').convert_alpha()
+        self.image_hit = pygame.image.load('images/enemy3_hit.png').convert_alpha()
         self.destroy_images = []
         self.destroy_images.extend([pygame.image.load('images/enemy3_down1.png').convert_alpha(), pygame.image.load('images/enemy3_down2.png').convert_alpha(), pygame.image.load('images/enemy3_down3.png').convert_alpha(), pygame.image.load('images/enemy3_down4.png').convert_alpha(), pygame.image.load('images/enemy3_down5.png').convert_alpha(), pygame.image.load('images/enemy3_down6.png').convert_alpha()])
         self.rect = self.image1.get_rect()
@@ -65,6 +76,8 @@ class BigEnermy(pygame.sprite.Sprite):
         self.active = True
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-15 * self.height, -5 * self.height)
         self.mask = pygame.mask.from_surface(self.image1)
+        self.energy = BigEnermy.energy
+        self.hit = False
         
     def move(self):
         if self.rect.top < self.height:
@@ -74,4 +87,5 @@ class BigEnermy(pygame.sprite.Sprite):
             
     def reset(self):
         self.active = True
+        self.energy = BigEnermy.energy
         self.rect.left, self.rect.top = randint(0, self.width - self.rect.width), randint(-15 * self.height, -5 * self.height)
